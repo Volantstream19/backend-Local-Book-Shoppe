bb0c3f2b02b8212b2584807133e061c6c1b4b9ec
King Kong <king-kong@gmail.com>
2022-11-08 16:06:14 -0800

    GET /books route returns list of books

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -1,15 +1,23 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const { bookData } = require('../lib/book-data.js');
+
+describe('books routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('should return a list of books', async () => {
+    const res = await request(app).get('/books');
+    const exp = bookData.map((book) => {
+      return { id: book.id, title: book.title, released: book.released };
+    });
+    expect(res.body).toEqual(exp);
   });
+
   afterAll(() => {
     pool.end();
   });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,44 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS books;
+
+DROP TABLE IF EXISTS authors;
+
+CREATE TABLE books (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR,
+    released VARCHAR
+);
+
+CREATE TABLE authors (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR,
+    dob VARCHAR,
+    pob VARCHAR
+);
+
+INSERT INTO
+    books (title, released)
+VALUES
+    ('Harry Potter', 1998),
+    ('Lord of the Rings', 1954),
+    ('Hobbit', 1937),
+    ('Fable Haven', 2006),
+    ('To kill a Mockingbird', 1998);
+
+INSERT INTO
+    authors (name, dob, pob)
+VALUES
+    ('J. K. Rowling', 1965, 'Yate, United Kingdom'),
+    (
+        'J. R. R. Tolkien',
+        1892,
+        'Bloemfontein, South Africa'
+    ),
+    (
+        'J. R. R. Tolkien',
+        1892,
+        'Bloemfontein, South Africa'
+    ),
+    ('Brandon Mull', 1974, 'Utah'),
+    ('Harper Lee', 1926, 'Monroeville, Alabama');
